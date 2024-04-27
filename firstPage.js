c2732c3924c1b4d052d4037f4ca4ae75489ca573
Carylen <irham.atmoko@ids.id>
2024-04-28 02:42:48 +0700

    first commit

---
+++ firstPage.js
@@ -0,0 +1,115 @@
+
+const ctx = document.getElementById("myChart");
+const cfg = document.getElementById("myChart2");
+const cfg2 = document.getElementById("myChart3");
+const data = [{x: 'Jan', net: 100, cogs: 50, gm: 50}, {x: 'Feb', net: 120, cogs: 55, gm: 75}, {x: 'Feb', net: 120, cogs: 55, gm: 75}];
+// chart.canvas.
+//   new Chart(ctx, {
+//     type: "bar",
+//     data: {
+//       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//       datasets: [
+//         {
+//           label: "# of Votes",
+//           data: [12, 19, 3, 5, 2, 3],
+//           borderWidth: 1,
+//         },
+//       ],
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//         },
+//       },
+//     },
+//   });
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb'],
+        hoverOffset: 4,
+        datasets: [
+            {
+                label: 'Net sales',
+                data: data,
+                    parsing: {
+                        yAxisKey: 'net'
+                    }
+            }, 
+            {
+                label: 'Cost of goods sold',
+                data: data,
+                parsing: {
+                    yAxisKey: 'cogs'
+                }
+            }, 
+            {
+                label: 'Gross margin',
+                data: data,
+                parsing: {
+                    yAxisKey: 'gm'
+                }
+            },
+        ]
+    },
+});
+
+new Chart(cfg,
+    {
+        type: 'line',
+        data: {
+            labels: ['One', 'Two', 'TR'],
+            datasets: [
+                {
+                    label: 'Net sales',
+                    data: data,
+                        parsing: {
+                            yAxisKey: 'net'
+                        }
+                }, 
+                {
+                    label: 'Cost of goods sold',
+                    data: data,
+                    parsing: {
+                        yAxisKey: 'cogs'
+                    }
+                }, 
+                {
+                    label: 'Gross margin',
+                    data: data,
+                    parsing: {
+                        yAxisKey: 'gm'
+                    }
+                },
+            ]
+        },
+    }  
+);
+
+new Chart(cfg2,
+    {
+        type: 'doughnut',
+        data: {
+            labels: ['Jan', 'Feb', 'Mar'],
+            datasets: [
+                {
+                    label: 'Net sales',
+                    data: data.map(x => x.net)
+                    // data: 
+                }
+                // {
+                //     label: 'Cost of goods sold',
+                //     data: data.map(x => x.net)
+                // }, 
+                // {
+                //     label: 'Gross margin',
+                //     data: data.map(x => x.net)
+                // },
+            ]
+        },
+    }  
+); 
+
+// let cfg = chart1
+
