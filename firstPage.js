3874ef584f3e3b7baaf02653f0ca267f488c4c5e
Carylen <irham.atmoko@ids.id>
2024-04-28 19:57:46 +0700

    update responsive css - customizing datasets commit

--- firstPage.js
+++ firstPage.js
@@ -1,4 +1,3 @@
-
 const ctx = document.getElementById("myChart");
 const cfg = document.getElementById("myChart2");
 const cfg2 = document.getElementById("myChart3");
@@ -7,8 +6,12 @@ const container = document.querySelector(".container");
 // container.innerHTML = '<div class="myChart"><canvas id="myChart"></canvas></div><div class="myChart"><canvas id="myChart2"></canvas></div><div class="myChart"><canvas id="myChart3"></canvas></div>'
 
 // container.appendChild([ctx, cfg, cfg2])
-        
-const data = [{x: 'Jan', net: 100, cogs: 50, gm: 50}, {x: 'Feb', net: 120, cogs: 55, gm: 75}, {x: 'Feb', net: 120, cogs: 55, gm: 75}];
+
+const data = [
+  { x: "Jan", Keisya: 100, Zahra: 10, Farahe: 20 },
+  { x: "Feb", Keisya: 60, Zahra: 155, Farahe: 15 },
+  { x: "Mar", Keisya: 30, Zahra: 195, Farahe: 15 },
+];
 // chart.canvas.
 //   new Chart(ctx, {
 //     type: "bar",
@@ -31,91 +34,100 @@ const data = [{x: 'Jan', net: 100, cogs: 50, gm: 50}, {x: 'Feb', net: 120, cogs:
 //     },
 //   });
 new Chart(ctx, {
-    type: 'bar',
+  type: "bar",
+  data: {
+    labels: data.map(x => x.x),
+    hoverOffset: 4,
+    datasets: [
+      {
+        label: "Keisya",
+        data: data,
+        parsing: {
+          yAxisKey: "Keisya",
+        },
+      },
+      {
+        label: "Zahra",
+        data: data,
+        parsing: {
+          yAxisKey: "Zahra",
+        },
+      },
+      {
+        label: "Farahe",
+        data: data,
+        parsing: {
+          yAxisKey: "Farahe",
+        },
+      },
+    ],
+  },
+});
+
+new Chart(cfg, {
+  type: "line",
+//   data: {
+//     labels: data.map(month => month.x),
+//     datasets: [
+//       {
+//         data: [10, 2, 0],
+//       },
+//     ],
+//   },
     data: {
-        labels: ['Jan', 'Feb'],
-        hoverOffset: 4,
+        labels: data.map((month) => month.x),
         datasets: [
             {
-                label: 'Net sales',
+            label: 'Keisya',
+            data: data,
+                parsing: {
+                    yAxisKey: 'Keisya'
+                },
+            },
+            {
+                label: 'Zahra',
                 data: data,
                     parsing: {
-                        yAxisKey: 'net'
+                        yAxisKey: 'Zahra'
+                    },
+            },
+            {
+                label: 'Farahe',
+                data: data, 
+                    parsing: {
+                        yAxisKey: 'Farahe'
                     }
-            }, 
-            {
-                label: 'Cost of goods sold',
-                data: data,
-                parsing: {
-                    yAxisKey: 'cogs'
-                }
-            }, 
-            {
-                label: 'Gross margin',
-                data: data,
-                parsing: {
-                    yAxisKey: 'gm'
-                }
             },
         ]
     },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
 });
 
-new Chart(cfg,
-    {
-        type: 'line',
-        data: {
-            labels: ['One', 'Two', 'TR'],
-            datasets: [
-                {
-                    label: 'Net sales',
-                    data: data,
-                        parsing: {
-                            yAxisKey: 'net'
-                        }
-                }, 
-                {
-                    label: 'Cost of goods sold',
-                    data: data,
-                    parsing: {
-                        yAxisKey: 'cogs'
-                    }
-                }, 
-                {
-                    label: 'Gross margin',
-                    data: data,
-                    parsing: {
-                        yAxisKey: 'gm'
-                    }
-                },
-            ]
-        },
-    }  
-);
-
-new Chart(cfg2,
-    {
-        type: 'doughnut',
-        data: {
-            labels: ['Jan', 'Feb', 'Mar'],
-            datasets: [
-                {
-                    label: 'Net sales',
-                    data: data.map(x => x.net)
-                    // data: 
-                }
-                // {
-                //     label: 'Cost of goods sold',
-                //     data: data.map(x => x.net)
-                // }, 
-                // {
-                //     label: 'Gross margin',
-                //     data: data.map(x => x.net)
-                // },
-            ]
-        },
-    }  
-); 
+new Chart(cfg2, {
+  type: "doughnut",
+  data: {
+    labels: data.map((month) => month.x),
+    datasets: [
+      {
+        label: "Keisya",
+        data: data.map((x) => x.Keisya),
+      },
+      {
+        label: "Zahra",
+        data: data.map((x) => x.Zahra),
+      },
+      {
+        label: "Farahe",
+        data: data.map((x) => x.Farahe),
+      },
+    ],
+  },
+});
 
 // let cfg = chart1
-
