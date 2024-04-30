79bab621f399d9aad96a0cf46f7aad5987f4af02
Carylen <irham.atmoko@ids.id>
2024-05-01 00:28:10 +0700

    add options for responsive - commit

--- firstPage.js
+++ firstPage.js
@@ -1,64 +1,66 @@
 const cfg = document.getElementById("myChart");
 const dropdown = document.getElementById("dropdown");
-const container = document.querySelector(".container");
-const title = document.getElementById("title")
+// const container = document.querySelector(".container");
+const title = document.getElementById("title");
 // import {Chart} from 'chart.js'
 
-title.textContent = dropdown.value + " Chart"
+title.textContent = dropdown.value + " Chart";
 
-
-const data = [
+const datappl = [
   { x: "Jan", Keisya: 100, Zahra: 10, Farahe: 20 },
   { x: "Feb", Keisya: 60, Zahra: 155, Farahe: 15 },
   { x: "Mar", Keisya: 30, Zahra: 195, Farahe: 15 },
 ];
 
-dropdown.addEventListener("change", () => {
-    // calling function for change 'TypeOfChart'
-    changeType(dropdown.value)
-    // calling function for change Title on Card, based on 'type of chart'
-    changeTitle(dropdown.value)
-  let con = dropdown.value;
-  console.log(con);
-});
-
-const changeTitle = (value) => {
-    title.textContent = value + " Chart"
+const changeType = (chart, value) => {
+  if (chart.config.type != value) {
+    console.log('before :', chart.config.type)
+    const type = `${value}`
+    chart.config.type = type
+  }
+  chart.update()
+  console.log('now :', chart.config.type)
 }
 
-const changeType = (value) => {
-  if (chart1.config.type != value) {
-    const type = `${value}`;
-    chart1.config.type = type;
-    chart1.update();
-    chart1.resize()
-  }
-//   console.log(chart1.config.type);
-};
+dropdown.addEventListener('change', () => {
+  changeType(chart1, dropdown.value)
+  title.textContent = dropdown.value + " Chart"
+})
 
 const chart1 = new Chart(cfg, {
-  type: "doughnut",
+  type: "bar",
   data: {
-    labels: data.map((month) => month.x),
+    labels: datappl.map((month) => month.x),
     datasets: [
       {
         label: "Keisya",
-        data: data.map((x) => x.Keisya),
+        data: datappl.map((x) => x.Keisya),
+        // data: datappl,
+        // parsing: {
+        //   yAxisKey: "Keisya",
+        // },
       },
       {
         label: "Zahra",
-        data: data.map((x) => x.Zahra),
+        data: datappl.map((x) => x.Zahra),
+        // data: datappl,
+        // parsing: {
+        //   yAxisKey: "Zahra",
+        // },
       },
       {
         label: "Farahe",
-        data: data.map((x) => x.Farahe),
+        data: datappl.map((x) => x.Farahe),
+        // data: datappl,
+        // parsing: {
+        //   yAxisKey: "Farahe",
+        // },
       },
     ],
   },
+  options: {
+    maintainAspectRatio: true,
+    responsive: true,
+  },
 });
 
-// window.addEventListener("afterprint", () => {
-//   chart1.resize();
-// });
-
-// let cfg = chart1
